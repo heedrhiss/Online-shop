b448e5671738986cf9f9ccee2d411d2162a3371f
heedrhiss <muyideenidris77@gmail.com>
2024-11-05 15:02:20 +0100

    update data

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,8 +1,15 @@
 
 export default function About() {
   return (
+    <>
     <h1>
-      About us
+      About Us
     </h1>
+    <p>
+      We are a small online store that specializes in selling fashions and other goods. We are located in the heart of the city, and we are open 24/7. We are here to help you find exactly what you are looking for. We are here to help you find exactly what you are looking for.
+    </p>
+    <p>Contact us on +234 909 307 6633</p>
+    <p>Email us @ muyideenidris77@gmail.com</p>
+    </>
   )
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,14 @@
 
 export default function Home() {
   return (
-    <h1>
-      Welcome Home...!
-    </h1>
+    <>
+    <h1 className="ms-auto fw-semibold text-center mb-3 fs-3">
+      Welcome to Heedrhiss Online Store– where style meets quality!
+      </h1>
+<p className="ms-auto fw-light text-center fs-5">At Heedrhiss Store, we believe in the power of fashion to elevate your lifestyle. Our online store is more than just a place to buy fashion essentials, home goods, tech accessories.
+We’re here to help you find exactly what you’re looking for. Dive in and enjoy an effortless shopping experience with fast shipping, easy returns, and customer support that's just a click away.
+
+Happy Shopping!</p>
+</>
   )
 }
